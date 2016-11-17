d77494e091db3e9bcfa0e3752e50d9405d587b59
charity5 <ls3201@columbia.com>
2016-11-16 16:11:22 -0800

    assigment4

---
+++ mod4_assignment/SpeakHello.js
@@ -0,0 +1,10 @@
+(function(window) {
+	var helloSpeaker = {};
+	window.helloSpeaker = helloSpeaker;
+
+	var speakWord = "Hello";
+	helloSpeaker.speak = function (name) {
+	  console.log(speakWord + " " + name);
+	}
+
+})(window);
